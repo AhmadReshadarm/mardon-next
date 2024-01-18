07dc68c58aa588476a07dfceaf29e31f339ab705
ARM <armreshad@gmail.com>
2024-01-18 15:20:05 +0300

    exp#8

--- ui-kit/Editor/index.tsx
+++ ui-kit/Editor/index.tsx
@@ -13,8 +13,8 @@ const FroalaEditor = dynamic(
   async () => {
     const values = await Promise.all([
       import('react-froala-wysiwyg'),
-      import('froala-editor/css/froala_style.min.css'),
-      import('froala-editor/css/froala_editor.pkgd.min.css'),
+      // import('froala-editor/css/froala_style.min.css'),
+      // import('froala-editor/css/froala_editor.pkgd.min.css'),
       import('froala-editor/js/plugins.pkgd.min.js'),
       import('froala-editor/js/plugins/image.min.js'),
       import('froala-editor/js/froala_editor.pkgd.min.js'),
