61af8a90771c2d7f08e2b08d4ec88ccc205a7e8d
ARM <armreshad@gmail.com>
2024-10-19 11:11:33 +0300

    disabled bundle analyzer for production mode

--- next.config.js
+++ next.config.js
@@ -73,12 +73,12 @@ module.exports = {
   },
   reactStrictMode: true,
 };
+// Production mode is On
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// });
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = withBundleAnalyzer(nextConfig);
+// module.exports = withBundleAnalyzer(nextConfig);
