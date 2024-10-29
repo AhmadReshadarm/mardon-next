01f7876dfde8241ed91d49e61dba933e4f4eafcf
ARM <armreshad@gmail.com>
2024-10-29 12:47:52 +0300

    stylized order email

--- ui-kit/Subscribers/helpers.ts
+++ ui-kit/Subscribers/helpers.ts
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { openErrorNotification } from 'common/helpers';
 import { useState } from 'react';
 import {
@@ -36,30 +35,25 @@ const handleSubscriber =
     dispatch(createSubscriber({ name, email }));
   };
 
-const getBase64Image = async (imageUrl) => {
-  const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-  const buffer = Buffer.from(response.data, 'binary');
-  const base64Image = buffer.toString('base64');
-  return `data:image/png;base64,${base64Image}`; // Adjust the MIME type as needed
-};
-
 const handleAdminCall =
   (name: string, phone: string, dispatch: AppDispatch) => async () => {
     if (isEmpty(phone))
       return openErrorNotification('Номер телефона не может быть пустым');
     if (isEmpty(name)) return openErrorNotification('имя не может быть пустым');
-    // const imageUrl = 'https://nbhoz.ru/static/NBHOZ_LOGO.png';
-    // // const imageUrl = 'http://localhost:3000/static/NBHOZ_LOGO.png';
-    // const base64Image = await getBase64Image(imageUrl);
+
     dispatch(
       sendAdminCallEmail({
-        to: 'info@nbhoz.ru',
+        to: 'NBHOZ <info@nbhoz.ru>',
         subject: `${name} просит перезвонить`,
         html: `
-        <div style="width:100%; display: flex; flex-direction: row; align-items:center; justify-content:center; padding-top:30px; padding-bottom:30px; background-color:#FDE5AC; border-radius: 20px;">
+        <div style="width:100%;">
+        <div style="width:90%; display: flex; flex-direction:row; justify-content:center; align-items:center;  padding: 30px 40px 30px 40px; background-color:#FDE5AC; border-radius: 20px;">
         <a href="https://nbhoz.ru" target="__blank"><img src="https://nbhoz.ru/static/NBHOZ_LOGO.png" alt="NBHOZ"></a>
         </div>
-        <div style="padding:20px 0px 20px 0px" >имя: ${name}, Номер телефона: ${phone}</div>`,
+        <div style="padding:20px 0px 20px 0px" >Имя: ${name}</div>
+        <div style="padding:20px 0px 20px 0px" >Номер телефона: ${phone}</div>
+        </div>
+        `,
       }),
     );
   };
