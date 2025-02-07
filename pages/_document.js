4ffea03e193b667280560c54ab004b5c8d3bf27d
ARM <armreshad@gmail.com>
2025-02-07 13:21:51 +0300

    visual improvment on the banner

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { useEffect } from 'react';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -8,13 +7,6 @@ class MyDocument extends Document {
   }
 
   render() {
-    let isMetrics = false;
-    useEffect(() => {
-      setTimeout(() => {
-        isMetrics = true;
-      }, 15000);
-    }, []);
-
     return (
       <Html>
         <Head>
